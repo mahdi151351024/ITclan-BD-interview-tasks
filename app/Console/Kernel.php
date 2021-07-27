cde07046eb5b2305d4fafdf6ddcbe10526fb5c26
mahdi351024 <mahdiodesk2015@gmail.com>
2021-07-28 01:56:57 +0600

    change scheduler code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\StartTournament::class
+        
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('tournament:start')
-                ->everyMinute();
+
     }
 
     /**
--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -19,8 +19,9 @@ class IdeaController extends Controller
 
     public function startTournament()
     {
-        $ideas = Idea::where('win', 'no')->get();
-        if($ideas->count() >= 8)
+        $ideas = Idea::where('win', 'no')->where('in_tournament', 'no')->get();
+        $running_tournament = Idea::where('in_tournament', 'yes')->get();
+        if($ideas->count() >= 8 && $running_tournament->count() == 0)
         {
             $ideas = Idea::where('win', 'no')->take(8)->get();
             foreach($ideas as $single_idea)
@@ -28,12 +29,51 @@ class IdeaController extends Controller
                 $single_idea->in_tournament = 'yes';
                 $single_idea->update();
             }
-            $this->winUsers();
+            
         }
-    }
 
-    public function winUsers()
-    {
-        \Artisan::call('schedule:work');
+        $ideas = Idea::where('win', 'no')->where('in_tournament', 'yes')->take(8)->get();
+        $ideas_a = Idea::where('win', 'no')->where('in_tournament', 'yes')->take(8)->get();
+        if($ideas->count() != 8)
+        {
+            $ideas = Idea::where('win', 'yes')->where('in_tournament', 'yes')->get();
+            $ideas_a = Idea::where('win', 'yes')->where('in_tournament', 'yes')->get();
+        }
+        $ideas_a = $ideas_a->random($ideas->count()/2);
+        $ids = [];
+        foreach($ideas_a as $idea)
+        {
+            $ids[] = $idea->id;
+            $idea->win = 'yes';
+            $idea->update();
+            $data = [];
+            $to_name = $idea->name;
+            $to_email = $idea->email;
+            $from_email = \Config::get('mail.from.address');
+            $data = [
+                'winner_name' => $idea->name,
+                'winner_message' => $ideas_a->count() == 1?'Congratulations!! You are the winner of the tournament':'Congratulations!! you have won and you are in top '.$ideas_a->count()
+            ];
+            // \Mail::send('winner_email', $data, function($message) use ($to_name, $to_email, $from_email) {
+            //     $message->to($to_email, $to_name)
+            //     ->subject('Tournament info');
+            //     $message->from($from_email,'Tournament Reminder');
+            //     });
+        }
+        $fail_users = Idea::whereNotIn('id', $ids)->where('in_tournament', 'yes')->get();
+        foreach($fail_users as $user)
+        {
+            $user->win = 'failed';
+            $user->update();
+        }
+        if($ideas_a->count() == 1)
+        {
+            $finish_tournament = Idea::where('in_tournament', 'yes')->get();
+            foreach($finish_tournament as $tournament)
+            {
+                $tournament->in_tournament = 'finished';
+                $tournament->update();
+            }
+        }
     }
 }
